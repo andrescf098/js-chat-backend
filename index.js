a5b6dc5c59a4952c2684e933c00458b21e82e771
Andrés Cifuentes <andrescf@outlook.com>
2023-10-05 18:09:36 -0500

    backend chat en js

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const response = await axios.put(
+      "https://api.chatengine.io/users/",
+      {
+        username: username,
+        secret: username,
+        first_name: username,
+      },
+      { headers: { "private-key": "7c2e6b1b-d930-47bd-a085-b236991994d3" } }
+    );
+    return res.status(response.status).json(response.data);
+  } catch (error) {
+    return res.status(error.response.status).json(error.response.data);
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Escuchando en el puerto " + 3001);
+});
