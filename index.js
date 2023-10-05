06f97d7361e4701e869a2fbe77c70f535cc92580
Andrés Cifuentes <andrescf@outlook.com>
2023-10-05 18:18:20 -0500

    cambios en el cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: true }));
+app.use(cors());
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
