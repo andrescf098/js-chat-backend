86ee1516ddda13c6feb51cc9a4c5950d6fb0804b
Andrés Cifuentes <andrescf@outlook.com>
2023-10-05 18:47:36 -0500

    cambios cors

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const { default: axios } = require("axios");
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+const whiteListCORS = ["http://localhost:5173", FRONTEND_URL];
+const options = {
+  origin: (origin, callback) => {
+    if (whiteListCORS.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed"));
+    }
+  },
+};
+app.use(cors(options));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
